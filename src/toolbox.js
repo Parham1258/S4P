de8091cf47d583a124447505eca77f7098fc39b2
Parham <95162943+Parham1258@users.noreply.github.com>
2022-06-20 20:44:20 +0430

    Some Change

--- src/toolbox.js
+++ src/toolbox.js
@@ -520,7 +520,7 @@ ${toolbox_search_category}
 <block type="parham_replitdb_getallitems"/>
 </category>
 <category name="Parham" colour="#ff4800">
-<label text="TThis Category Remove Soon..."></label>
+<label text="This Category Remove Soon..."></label>
 <label text="🍔"></label>
 <block type="parham_hamburger"/>
 </category>
